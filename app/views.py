0971587119701a0d6145b9773e1983040a262723
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-11-25 19:41:38 +0700

    feat: add model integration with api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from gradio_client import Client
 
 # Create your views here.
+
+
+def index(request):
+    data = []
+    description = ""
+
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = 10
+
+        client = Client(
+            "https://yotakey-mammals-search.hf.space/--replicas/66lqq/")
+        result = client.predict(
+            description,
+            amount,
+            api_name="/predict"
+        )
+
+        data = result.split(",\n")
+
+    return render(request, "app/index.html", context={"datas": data, "description": description})
