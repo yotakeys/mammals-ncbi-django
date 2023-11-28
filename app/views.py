ce951996796660d6ad4bfe2bcfb86572958ce40c
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-11-28 18:20:33 +0700

    feat: add filetype hardcode to url_download

--- app/views.py
+++ app/views.py
@@ -8,10 +8,19 @@ from .models import Mammals
 def index(request):
     data = []
     description = ""
+    file_type = {
+        "Genome Sequences": "GENOME_FASTA",
+        "Annotation features (GFF)": "GENOME_GFF",
+        "Sequence and annotation (GBFF)": "GENOME_GBFF",
+        "Transcripts (FASTA)": "RNA_FASTA",
+        "Genomic coding sequences (FASTA)": "CDS_FASTA",
+        "Protein (FASTA)": "PROT_FASTA",
+        "Sequence report (JSONL)": "SEQUENCE_REPORT"
+    }
 
     if request.method == "POST":
         description = request.POST.get('description')
-        amount = 10
+        amount = 5
 
         client = Client(
             "https://yotakey-mammals-search.hf.space/--replicas/66lqq/")
@@ -26,4 +35,13 @@ def index(request):
         data = Mammals.objects.filter(
             organism_name__in=data_mammals)
 
-    return render(request, "app/index.html", context={"datas": data, "description": description})
+        filetype_used = "," + file_type['Genome Sequences'] \
+                            + "," + file_type['Sequence report (JSONL)']
+        for idx in range(len(data)):
+            temp = data[idx].url_download
+            index_filtype = temp.find('DEFAULT')
+            temp = temp[:index_filtype + len(
+                'DEFAULT')] + filetype_used + temp[index_filtype + len('DEFAULT'):]
+            data[idx].url_download = temp
+
+    return render(request, "app/index.html", context={"datas": data, "description": description, "filetype": file_type})
